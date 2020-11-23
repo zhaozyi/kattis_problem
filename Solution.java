d94e462c41b78cc964b818d02d7db1dbe5285663
zhaozyi <ZhaoZyi@utexas.edu>
2020-11-23 10:46:06 +0800

    update

---
+++ Solution.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        int b = input.nextInt();
+        System.out.println(flip(a) < flip(b) ? flip(b) : flip(a));
+    }
+
+    private static int flip(int n) {
+        int result = 0;
+        while (n != 0) {
+            result = result * 10 + n % 10;
+            n /= 10;
+        }
+        return result;
+    }
+}
